0227ce7a4ce9847ddd7e710f90c0a3a8e67734b4
Hiroshi Yamasaki <ymskhrs@gmail.com>
2021-04-25 15:48:38 +0900

    Update keymap.rb

--- keymap.rb
+++ keymap.rb
@@ -7,12 +7,13 @@ end
 kbd = Keyboard.new
 
 kbd.init_pins(
-  [6],   # row0, row1
-  [28]  # col0, col1
+  [6, 7],   # row0, row1
+  [28, 27]  # col0, col1
 )
 
 kbd.add_layer :default, [
   %i(KC_A),    %i(A)
+  %i(KC_B),    %i(B)
 ]
 
 kbd.start!

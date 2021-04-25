d8f644e40be903757a0124217f83bd16105f423e
Hiroshi Yamasaki <ymskhrs@gmail.com>
2021-04-25 15:13:55 +0900

    first commit

---
+++ keymap.rb
@@ -0,0 +1,18 @@
+# Wait until Keyboard class is ready
+while !$mutex
+  relinquish
+end
+
+# Initialize a Keyboard
+kbd = Keyboard.new
+
+kbd.init_pins(
+  [15],   # row0, row1
+  []  # col0, col1
+)
+
+kbd.add_layer :default, [
+  %i(KC_A),    %i(A)
+]
+
+kbd.start!
